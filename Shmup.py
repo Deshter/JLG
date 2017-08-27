ad56d68be73a20a95d529b4f913134d3b3e15ae3
Deshter <31319454+Deshter@users.noreply.github.com>
2017-08-28 02:23:18 +0400

    0.01
    hurr durr

---
+++ Shmup.py
@@ -0,0 +1,124 @@
+import pygame
+import random
+
+#Starting by setting up the screen dimensions
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+#And setting up Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+pygame.init() #revving up pygame
+pygame.mixer.init() #revving up sounds
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Shmoop")
+clock = pygame.time.Clock()
+collision_time = 0
+mercy = False
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+        self.speedy = 0
+        self.life = 3
+
+    def update(self):
+        self.speedx = 0
+        self.speedy = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_a]:
+            self.speedx = -8
+        if keystate[pygame.K_d]:
+            self.speedx = 8
+        if keystate[pygame.K_w]:
+            self.speedy = -8
+        if keystate[pygame.K_s]:
+            self.speedy = 8
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 5)
+        self.speedx = random.randrange(-5, 5)
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 16)
+
+all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+for i in range(8):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
+# Game loop
+running = True
+
+while running:
+    #keep loop running
+    clock.tick(FPS)
+    #inputs
+    for event in pygame.event.get():
+        #Check for closing window
+        if event.type == pygame.QUIT:
+                running = False
+
+    # Update
+    all_sprites.update()
+    #Check to see if a mob hit the player
+    hits = pygame.sprite.spritecollide(player, mobs, False)
+    if hits and mercy == False:
+        player.life -= 1
+        mercy = True
+        collision_time = pygame.time.get_ticks()
+
+    if mercy == True:
+        player.image.fill(WHITE)
+        if pygame.time.get_ticks() - collision_time > 3000:
+            mercy = False
+            player.image.fill(GREEN)
+
+    if player.life <= 0:
+        player.image.fill(BLACK)
+
+    #draw
+    screen.fill (BLACK)
+    all_sprites.draw(screen)
+    #lastly, renew the screenpygame.display.flip()
+    pygame.display.flip()
+
+pygame.quit
